589c1f3617508ad9ac35a9b73b0cc143f8514b55
alan9572 <alan.sagrario@eest3necochea.edu.ar>
2025-10-21 21:16:55 -0300

    fghjkl;

---
+++ cancha/cancha/Cancha.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cancha;
+using Microsoft.Data.Sqlite;
+
+namespace cancha
+{
+    internal class Cancha
+    {
+        protected int id_cancha;
+        protected string deporte;
+        protected int num_jugadores;
+        protected int turnos;
+        protected string nom_jugadores;
+
+    public void ActualizarBD() 
+        {
+            string consulta = "UPDATE cancha SET deporte=@deporte, turno=@turno, cliente=@cliente, num_jugadores=@num_jugadores WHERE id_cancha=@id";
+            List<SqliteParameter> parametros = new List<SqliteParameter>();
+            parametros.Add(new SqliteParameter("@deporte", 10));
+            parametros.Add(new SqliteParameter("@turno", 10));
+            parametros.Add(new SqliteParameter("@cliente", 10));
+            parametros.Add(new SqliteParameter("@num_jugadores", 10));
+            parametros.Add(new SqliteParameter("@id", 10));
+            GestorConexion.Instancia.ModificarBD(consulta, parametros);
+        }
+        public static List<Cancha> traertodas()
+        {
+            string consulta = "SELECT * FROM cancha";
+            DataTable tabla = GestorConexion.Instancia.ConsultarBD(consulta, null);
+            foreach (DataRow fila in tabla.Rows)
+            {
+                Cancha nuevoObjeto= new Cancha();
+                nuevoObjeto.atributo1 = fila["columna1"].ToString();
+                nuevoObjeto.atributo1 = int.Parse(fila["columna2"].ToString());
+                lista.add(nuevoObjeto);
+            }
+            return lista;
+        }
+        public static Cancha traeruno(int idAbuscar);
+
+    }
+
+}
---
+++ cancha/cancha/GestorConexion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace cancha
+{
+    internal class GestorConexion
+    {
+        private GestorConexion() {
+            conexion = new SqliteConnection("datasource= cancah.db");
+
+        }
+        private static GestorConexion _instancia;
+        public static GestorConexion Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                    _instancia = new GestorConexion();
+                return _instancia;
+            }
+            private set { }
+        }
+        private SqliteConnection conexion;
+        private SqliteCommand comando;
+        private SqliteDataReader lector;
+        public void ModificarBD(string consulta, List<SqliteParameter> parametros)
+        {
+            try
+            {
+                conexion.Open();
+                comando = new SqliteCommand(consulta, conexion);
+                comando = new SqliteCommand(consulta, conexion);
+                if (parametros != null)
+                    foreach (SqliteParameter p in parametros)
+                        comando.Parameters.Add(p);
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            
+            }
+            conexion.Close();
+        }
+        public DataTable ConsultarBD(string consulta, List<SqliteParameter> parametros)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                conexion.Open();
+                comando = new SqliteCommand(consulta, conexion);
+                if (parametros != null)
+                    foreach (SqliteParameter p in parametros)
+                        comando.Parameters.Add(p);
+                lector=comando.ExecuteReader();
+                tabla.Load(lector);
+            }
+            catch (Exception e)
+
+            {
+                MessageBox.Show(e.Message);
+
+            }
+            conexion.Close();
+            return (tabla);
+        }
+    }
+}
